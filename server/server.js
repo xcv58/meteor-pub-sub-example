4aa60d6d9747cf1cfe2ddeac91ec6330afa05139
xcv58 <chenyihonglove@gmail.com>
2015-10-28 20:04:24 -0400

    add default publish items.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 Meteor.startup(function() {
+  var mid = 5;
   if (Lists.find().fetch().length == 0) {
     for (var i = 0; i < 10; i++) {
       Lists.insert({'index': i});
@@ -8,12 +9,11 @@ Meteor.startup(function() {
   Meteor.publish('lists', function(n) {
     console.log('publish');
     if (n === -1) {
-      return [];
-    }
-    if (n) {
-      var cursor = Lists.find({ index: { $gt: 5 } });
+      var cursor = Lists.find({ index: { $eq: mid } });
+    } else if (n) {
+      var cursor = Lists.find({ index: { $gt: mid } });
     } else {
-      var cursor = Lists.find({ index: { $lt: 5 } });
+      var cursor = Lists.find({ index: { $lt: mid } });
     }
     console.log(cursor.fetch());
     return cursor;
